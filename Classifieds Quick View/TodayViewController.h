69063ac3354db1898fde8af77a4e1034b95c1a76
Chanced270 <zackary.divine@gmail.com>
2015-11-01 23:33:52 -0600

    fixed settings view controller

---
+++ Classifieds Quick View/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  Classifieds Quick View
+//
+//  Created by Zackary Divine on 10/7/15.
+//  Copyright © 2015 Zackary Divine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
